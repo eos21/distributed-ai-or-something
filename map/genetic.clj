5f2e897b5f152f673ea11ffa3d60134f94e3b0d2
redaphid <iam@aaronherres.com>
2018-01-08 18:48:15 -0700

    epochs, mutate numbers

--- map/genetic.clj
+++ map/genetic.clj
@@ -1,13 +1,7 @@
 (def operations '(+ - * /))
-(def mutation-rate 0.5)
+(def mutation-rate 0.05)
 (def population-size 100)
 (def gene '(fn [x]))
-
-(def population '(
-  (+ x 10)
-  (- x 2 3)
-  (* x 1)
-))
 
 (defn rand-gene [genes]
   (if (empty? genes) nil (rand-nth genes))
@@ -17,12 +11,36 @@
   (rand-gene (remove nil? args))
 )
 
+(defn mutate-gene [gene]
+  (println "mutating" gene)
+  (if (some #(= gene %) operations)
+    (rand-nth operations)
+    (if (number? gene)
+      (if (> (rand) 0.5)
+        (- gene 1)
+        (+ gene 1)
+      )
+      gene
+    )
+  )
+)
+
+(defn possibly-mutate-gene [gene]
+  (if (> (rand) mutation-rate)
+  gene
+  (mutate-gene gene))
+)
+
+(defn mutate [creature]
+  (map possibly-mutate-gene creature)
+)
+
 (defn breed [population]
   (let [
       dad (pad 100 (rand-nth population) nil)
       mom (pad 100 (rand-nth population) nil)
     ]
-    (remove nil? (map pick-gene dad mom))
+    (mutate (remove nil? (map pick-gene dad mom)))
   )
 )
 
@@ -33,9 +51,17 @@
   ((eval (concat gene [creature])) 1)
 )
 
-(def population (distinct (concat population
-  (take population-size (repeatedly #(breed population))))))
+(defn epoch [population]
+  (let [new-population (distinct (concat population (take population-size (repeatedly #(breed population)))))]
+    (take (/ population-size 10) (reverse (sort-by fitness new-population)))
+  )
+)
 
-(println (map fitness population))
+(def population '(
+  (+ x 10)
+  (- x 2 3)
+  (* x 1)
+))
+
+(def population (epoch population))
 (println population)
-(def population (take (/ population-size 10) (reverse (sort-by fitness population))))
