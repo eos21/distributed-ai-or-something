cd8b2f16b83e3d5b89034e59e535cc239887ac3f
Roy van de Water <roy.vwater@gmail.com>
2018-01-15 12:02:31 -0700

    Bash

--- genetics-map/manual-run.sh
+++ genetics-map/manual-run.sh
@@ -1,3 +1,13 @@
-# EQUATION=zdpuAuH6qA5dqPC89LyTUgbkJLxyGuKDYW57DYNmPzCs6fb7E # x^3
-EQUATION=zdpuAmtKj6ZTeXQGNfS1htFoUytkEXYcrJzB58J43ryroeNU3 # 2x^2+7
-ipfs dag get $EQUATION | env LEIN_FAST_TRAMPOLINE=y lein trampoline run
+#!/bin/bash
+
+# INPUT=zdpuAuH6qA5dqPC89LyTUgbkJLxyGuKDYW57DYNmPzCs6fb7E # x^3
+# INPUT=zdpuAogbJ8X93BZuucoPEem1GGnmYUet5DsTGfcyrypWXahxo # x^3 - x
+INPUT=zdpuAmtKj6ZTeXQGNfS1htFoUytkEXYcrJzB58J43ryroeNU3 # 2x^2+7
+
+main() {
+  set -e -o pipefail
+
+  ipfs dag get "$INPUT" \
+  | env LEIN_FAST_TRAMPOLINE=y lein trampoline run
+}
+main "$@"
